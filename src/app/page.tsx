139deb385b20ba4c955487335327a5d924044dd1
aminulsujon <aminul532sujon@gmail.com>
2024-11-14 17:11:31 +0600

    Run vpn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,9 +10,20 @@ export default function Home() {
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://www.muskvpn.com/"
           >
-            
+            Free VPN for Android
           </a>
-         
+          <br/>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://play.google.com/store/apps/details?id=com.muskvpn.muskvpn"
+          >
+            Download Free VPN for Android
+          </a>
+          <br/>
+          <img width={500} src="Free-VPN-Adroid.jpg" />
+          <br/>
+
+          <a href="https://www.muskvpn.com">&copy; muskvpn.com</a>
         </div>
       </main>
       
